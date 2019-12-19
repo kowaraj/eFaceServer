bd3b25357a5a0890ef074bb3aa5a46fe017cad40
kowaraj <kowaraj4stuff@gmail.com>
2019-12-19 18:54:05 +0100

    Revert "Wrong commit message - Moved replies out to the FidzStatus class"
    This reverts commit fb8d09121cf7a67c7e7e653db77568c88acdf5e3.

--- server_mt.py
+++ server_mt.py
@@ -7,7 +7,6 @@ import FidzStatus
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        print('\n1. Request received.............')
         fs = FidzStatus.Status()
 
         data = str(self.request.recv(1024))
@@ -25,7 +24,6 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         data = str(self.request.recv(1024))
         r = fs.getReplyOnMsgType6(data)
         self.request.sendall(r)
-        print('\n4. Request completed............')
 
 
 class LegacyEAssServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
