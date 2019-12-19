fcf0322ac48cb789d4bec36b473cbfa58ce4bb04
kowaraj <kowaraj4stuff@gmail.com>
2019-12-19 18:31:03 +0100

    Moved replies out to the FidzStatus class

--- server_mt.py
+++ server_mt.py
@@ -7,26 +7,22 @@ import FidzStatus
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        data = str(self.request.recv(1024))
-        print('request received: ' + data)
+        fs = FidzStatus.Status()
 
-        r = bytearray(b'eAS01\x00\x00\x00\x02\x00\x02\x00\x0b\x02\x00\x00\x00\x01\x00\x01\x00\x00\x01\x00\x00\x00\x00\x02\tSESSIONID\x02\x00\x00\x00\x01\x00\x00\x00\x00\x08\x00\x00\x00Welcome.')        
-        print('length = ' + str(len(r)))
+        data = str(self.request.recv(1024))
+        r = fs.getReplyOnMsgType1(data)
         self.request.sendall(r)
 
         print('\n2. Waiting to continue..........')
+
         data = str(self.request.recv(1024))
-        print('request received: ' + data)
-        r = bytearray(b'eAS0\x1a\x00\x00\x00\x02\x00\x02\x00\x0b\x02\x00\x00\x00\x05\x00\x01\x00\x00\x01\x00\x00\x00\x00\x02\x00\r\x02\x00\x00\x00\x01\x00')
-        print('length = ' + str(len(r)))
+        r = fs.getReplyOnMsgType5(data)
         self.request.sendall(r)
 
         print('\n3. Waiting to continue..........')
+
         data = str(self.request.recv(1024))
-        print('request received: ' + data)
-        
-        fs = FidzStatus.Status()
-        r = fs.getReport()
+        r = fs.getReplyOnMsgType6(data)
         self.request.sendall(r)
 
 
